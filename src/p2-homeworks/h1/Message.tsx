57c0b85aef2d44763ad916e9983f56de4c0edd16
andreiryaskov <andreiryaskov@gmail.com>
2021-11-10 15:54:38 +0300

    DZ_1

---
+++ src/p2-homeworks/h1/Message.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import style from './Message.module.css';
+import {MessageDataType} from "./HW1";
+
+function Message(props: MessageDataType) {
+    return (
+        <div className={style.message_wrapper}>
+            <img className={style.message_img} src={props.avatar} alt="Me"/>
+            <div className={style.message_textWrapper}>
+                <div className={style.message_name}>{props.name}</div>
+                <div className={style.message_text}>{props.message}</div>
+                <div className={style.message_time}>{props.time}</div>
+            </div>
+            <div className={style.message_smallBlock}></div>
+        </div>
+    )
+}
+
+export default Message
