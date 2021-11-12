d4f70ed8dac7ad707d4d6c11a02140b719adb360
andreiryaskov <andreiryaskov@gmail.com>
2021-11-12 16:18:11 +0300

    HW_12_11_append

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -6,12 +6,13 @@ function Message(props: MessageDataType) {
     return (
         <div className={style.message_wrapper}>
             <img className={style.message_img} src={props.avatar} alt="Me"/>
+            <div className={style.message_smallBlock}></div>
             <div className={style.message_textWrapper}>
                 <div className={style.message_name}>{props.name}</div>
                 <div className={style.message_text}>{props.message}</div>
                 <div className={style.message_time}>{props.time}</div>
             </div>
-            <div className={style.message_smallBlock}></div>
+
         </div>
     )
 }
