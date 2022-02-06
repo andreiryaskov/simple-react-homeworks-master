487cdadfc692887d8b186c7c565029b516abd101
andreiryaskov <andreiryaskov@gmail.com>
2022-02-06 18:52:10 +0300

    HW9

--- src/p1-main/m1-ui/u1-app/App.tsx
+++ src/p1-main/m1-ui/u1-app/App.tsx
@@ -4,19 +4,13 @@ import HW5 from "../../../p2-homeworks/h5/HW5";
 import HW6 from "../../../p2-homeworks/h6/HW6";
 import HW7 from "../../../p2-homeworks/h7/HW7";
 import HW8 from "../../../p2-homeworks/h8/HW8";
+import Junior from "../../../p2-homeworks/h5/pages/Junior";
 
 function App() {
     return (
         <div className={style.App}>
             <div><h1>React homeworks:</h1></div>
-            {/*<HW1/>*/}
-            {/*<HW2/>*/}
-            {/*<HW3/>*/}
-            {/*<HW4/>*/}
             <HW5/>
-            <HW6/>
-            <HW7/>
-            <HW8/>
         </div>
     )
 }
--- src/p2-homeworks/h5/pages/Junior.tsx
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import s from './../Header.module.css';
+import HW7 from "../../h7/HW7";
+import HW8 from "../../h8/HW8";
+import HW9 from "../../h9/HW9";
 
 function Junior() {
     return (
         <div>
             <h1>JUNIOR</h1>
-            <img className={s.jun_img}
-                 src="https://maiso.ru/files/user/287320/board/njanja-na-chasbebisiter.jpg"
-                 alt="Jun"/>
+            <HW7/>
+            <HW8/>
+            <HW9/>
         </div>
     )
 }
